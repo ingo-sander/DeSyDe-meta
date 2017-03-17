78566e8ebac750b0e665b89d28ef6d08c8378319
nkhal <nkhal@kth.se>
2017-03-17 16:52:10 +0100

    new GA updated for multiobj

--- src/metaheuristic/ga_population.cpp
+++ src/metaheuristic/ga_population.cpp
@@ -47,27 +47,49 @@ std::ostream& operator<< (std::ostream &out, const GA_Population &pop)
 
 void GA_Population::update(int t_id)
 {
-    int start_id = t_id * individual_per_thread/2;
-    int end_id = start_id + individual_per_thread/2;
+    int start_id = t_id * individual_per_thread;
+    int end_id = start_id + individual_per_thread;
     if(t_id == no_threads -1)///Last thread takes care of all remaining particles
-        end_id = no_individulas/2-1;
-    for(int i=start_id;i<end_id;i+=2)    
+        end_id = no_individulas-1;
+    if(cfg.settings().multi_obj)
     {
-        /// Copy parent one because it will be updated
-        shared_ptr<Chromosome> child1(new Chromosome(*population[parents[i].first]));
-        
-        shared_ptr<Chromosome> child2(new Chromosome(*population[parents[i].second]));
-        
-        child1->set_best_global(population[parents[i].second]->get_current_position());
-        child1->update();
-        
-        child2->set_best_global(population[parents[i].first]->get_current_position());
-        child2->update();
-        
-        next_population[i] = child1;        
-        next_population[i+1] = child2;
+        shared_ptr<Chromosome> parent1;
+        shared_ptr<Chromosome> parent2;;
+                
+        for(int i=start_id;i<end_id;i++)    
+        {
+            if(!par_f.pareto.empty())
+            {
+                int par_indx = random::random_indx(par_f.pareto.size()-1);        
+                population[i]->set_best_global(par_f.pareto[par_indx]);                
+            }
+            else
+            {
+                population[i]->set_best_global(population[random::random_indx(population.size()-1)]->get_current_position());                
+            }    
+            population[i]->update();
+        }
     }
-    
+    else
+    {
+        for(int i=start_id;i+1<end_id;i+=2)    
+        {
+            int p_i = i/2;
+            /// Copy parent one because it will be updated
+            shared_ptr<Chromosome> child1(new Chromosome(*population[parents[p_i].first]));
+            
+            shared_ptr<Chromosome> child2(new Chromosome(*population[parents[p_i].second]));
+            
+            child1->set_best_global(population[parents[p_i].second]->get_current_position());
+            child1->update();
+            
+            child2->set_best_global(population[parents[p_i].first]->get_current_position());
+            child2->update();
+            
+            next_population[i] = child1;        
+            next_population[i+1] = child2;
+        }
+    }
 }
 
 int GA_Population::no_converged_individuals()
@@ -102,14 +124,18 @@ void GA_Population::select_fittest()
 void GA_Population::new_population()
 {
     //select_fittest();
+    if(cfg.settings().multi_obj)
+        return;
     population = next_population;
 }
 void GA_Population::sort_population()
 {
+    
+    if(cfg.settings().multi_obj)
+        return;
     std::sort(population.begin(), population.end(),
                     [](shared_ptr<Chromosome> const a, shared_ptr<Chromosome> const b) -> bool 
                     { return a->dominate(b); } );    
-    
     //We also slecet parents here
     parents.clear();
     vector<int> all_indices;
@@ -122,5 +148,5 @@ void GA_Population::sort_population()
         int indx = all_indices[rand];
         parents.push_back(possible_parents[indx]);
         all_indices.erase(all_indices.begin()+rand);
-    }     
+    }         
 }
